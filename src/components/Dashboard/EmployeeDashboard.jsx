d70bed1b972da7e2e6b46aac29ea51ca0cbb5237
Gunjan vaishnav <gautamsharmayyy@gmail.com>
2025-01-20 10:42:33 +0530

    change3

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import Header from '../other/Header'
 import TaskListNumbers from '../other/TaskListNumber'
-import TaskList from '../TaskList/TaskList'
-
+import TaskList from "../Tasklist/TaskList"
 const EmployeeDashboard = (props) => {
 
   return (
